947de2c0a5ffc1b924f5d306a723dd7769cfc34f
Yamato Kataoka <yk_time@icloud.com>
2019-09-16 11:30:32 +0900

    Finished fileWithColdestTemperature method on Programming Exercise: Parsing Weather Data

--- week3/ParsingWeatherData/ParsingWeatherData.java
+++ week3/ParsingWeatherData/ParsingWeatherData.java
@@ -32,6 +32,32 @@ public class ParsingWeatherData {
         return coldestSoFar;
     }
     
+    // Write the method fileWithColdestTemperature that has no parameters. This method 
+    // should return a string that is the name of the file from selected files that 
+    // has the coldest temperature.
+    private String fileWithColdestTemperature () {
+        DirectoryResource dr = new DirectoryResource();
+        CSVRecord coldestSoFar = null;
+        String coldestDay = "";
+        for (File f : dr.selectedFiles()) {
+            FileResource fr = new FileResource(f);
+            CSVRecord currentRow = coldestHourInFile(fr.getCSVParser());
+            if (coldestSoFar == null) {
+                coldestSoFar = currentRow;
+            }
+            else {
+                double coldestTemp = Double.parseDouble(coldestSoFar.get("TemperatureF"));
+                double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
+                
+                if (coldestTemp > currentTemp && currentTemp != -9999) {
+                    coldestSoFar = currentRow;
+                    coldestDay = f.toString();
+                }
+            }
+        }
+        return coldestDay;
+    }
+    
     // You should also write a void method named testColdestHourInFile() to 
     // test this method and print out information
     public void testColdestHourInFile () {
@@ -46,5 +72,19 @@ public class ParsingWeatherData {
         coldest = coldestHourInFile(fr.getCSVParser());
         System.out.println("coldest temperature was " + coldest.get("TemperatureF") +
                             " at " + coldest.get("TimeEST"));
+    }
+    
+    // You should also write a void method named testFileWithColdestTemperature() 
+    // to test this method.
+    public void testFileWithColdestTemperature () {
+        String coldestDay = fileWithColdestTemperature();
+        System.out.println("Coldest day was in file " + coldestDay);
+        FileResource fr = new FileResource(coldestDay);
+        CSVRecord coldest = coldestHourInFile(fr.getCSVParser());
+        System.out.println("Coldest temperature on that day was " + coldest.get("TemperatureF"));
+        System.out.println("All the Temperatures on the coldest day were:");
+        for (CSVRecord current : fr.getCSVParser()) {
+            System.out.println( current.get("DateUTC") + ": " + current.get("TemperatureF"));
+        }
     }
 }
