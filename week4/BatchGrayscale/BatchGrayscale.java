71822a7d9eca2e9b87a8c68cf26716c0d387e98e
Yamato Kataoka <yk_time@icloud.com>
2019-09-25 08:41:04 +0900

    Finished to write makeGray method which convert picture to grayscale

--- week4/BatchGrayscale/BatchGrayscale.java
+++ week4/BatchGrayscale/BatchGrayscale.java
@@ -5,6 +5,25 @@
  * @author yamatokataoka
  * @version September 25th, 2019
  */
+
+import edu.duke.*;
+
 public class BatchGrayscale {
+    public ImageResource makeGray (ImageResource inImage) {
+        // create blank image
+        ImageResource outImage = new ImageResource(inImage.getWidth()
+                                                    , inImage.getHeight());
+        // convert RGB color into grayscale for each pixel
+        for (Pixel pixel : outImage.pixels()) {
+            Pixel inPixel = inImage.getPixel(pixel.getX(), pixel.getY());
+            int average = (inPixel.getRed() + inPixel.getGreen() 
+                            + inPixel.getBlue()) / 3;
+            pixel.setRed(average);
+            pixel.setGreen(average);
+            pixel.setBlue(average);
+        }
+        return outImage;
+    }
     
+    // write testing method for makeGray
 }
