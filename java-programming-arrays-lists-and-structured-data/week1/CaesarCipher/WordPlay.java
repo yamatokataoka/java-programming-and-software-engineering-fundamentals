fd6b478c37b517c1f1b90a69ade4318f64387f4a
Yamato Kataoka <yk_time@icloud.com>
2019-10-03 09:04:39 +0900

    Created Caesar Cipher project

---
+++ java-programming-arrays-lists-and-structured-data/week1/CaesarCipher/WordPlay.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of WordPlay here.
+ * 
+ * @author yamatokataoka
+ * @version 20191003
+ */
+public class WordPlay {
+    // Write a method isVowel that has one Char parameter named ch
+    // This method returns true if ch is a vowel (one of 'a', 'e', 'i', 'o', or 'u'
+    // or the uppercase versions) and false otherwise.
+}
