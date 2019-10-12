f7af4f51343ede429ffab09edc1355e0b9b933ac
Yamato Kataoka <yk_time@icloud.com>
2019-10-12 15:28:11 +0900

    finiahed Assignment 2: Caesar Cipher Two Keys Decrypt and test were passed on CaesarBreaker project

--- java-programming-arrays-lists-and-structured-data/week1/CaesarBreaker/CaesarBreaker.java
+++ java-programming-arrays-lists-and-structured-data/week1/CaesarBreaker/CaesarBreaker.java
@@ -5,6 +5,9 @@
  * @author yamatokataoka
  * @version October 11th, 2019
  */
+
+import edu.duke.*;
+
 public class CaesarBreaker {
     // Complete the decryption method shown in the lesson by creating a
     // CaesarBreaker class with the methods countLetters, maxIndex, and
@@ -14,10 +17,8 @@ public class CaesarBreaker {
         int[] freqs = countLetters(encrypted);
         int maxDex = maxIndex (freqs);
         int dkey = maxDex - 4;
-        System.out.println(dkey);
         if (maxDex < 4) {
             dkey = 26 - (4 - maxDex);
-            System.out.println(dkey);
         }
         return cc.encrypt(encrypted, 26 - dkey);
     }
@@ -47,6 +48,67 @@ public class CaesarBreaker {
         return maxIndex;
     }
     
+    // Write the method halfOfString in the CaesarBreaker class that has
+    // two parameters, a String parameter named message and an int parameter
+    // named start. This method should return a new String that is every
+    // other character from message starting with the start position.
+    public String halfOfString (String message, int start) {
+        StringBuilder sbMessage = new StringBuilder(message);
+        StringBuilder halfOfString = new StringBuilder();
+        for (int k = 0; k < sbMessage.length(); k++) {
+            if (start == 0 && k % 2 == start) {
+                halfOfString.append(sbMessage.charAt(k));
+            }
+            else if (start == 1 && k % 2 == start) {
+                halfOfString.append(sbMessage.charAt(k));
+            }
+        }
+        return halfOfString.toString();
+    }
+    
+    // Write the method getKey in the CaesarBreaker class that has one
+    // parameter, a String s.
+    // This method should call countLetters to get an array of the letter
+    // frequencies in String s and then use maxIndex to calculate the index
+    // of the largest letter frequency, which is the location of the
+    // encrypted letter ‘e’, which leads to the key, which is returned.
+    public int getKey (String s) {
+        int[] freqs = countLetters(s);
+        int maxDex = maxIndex (freqs);
+        int dkey = maxDex - 4;
+        if (maxDex < 4) {
+            dkey = 26 - (4 - maxDex);
+        }
+        
+        return dkey;
+    }
+    
+    // Write the method decryptTwoKeys in the CaesarBreaker class that has
+    // one parameter, a String parameter named encrypted that represents
+    // a String that was encrypted with the two key algorithm.
+    // This method attempts to determine the two keys used to encrypt the
+    // message, prints the two keys, and then returns the decrypted String
+    // with those two keys
+    public String decryptTwoKeys (String encrypted) {
+        CaesarCipher cc = new CaesarCipher();
+        // Calculate a String of every other character starting with the
+        // first character of the encrypted String by calling halfOfString.
+        String firstHalfOfString = halfOfString(encrypted, 0);
+        // Calculate a String of every other character starting with the
+        // second character of the encrypted String.
+        String secondHalfOfString = halfOfString(encrypted, 1);
+        // Then calculate the key used to encrypt each half String.
+        int firstDKey = getKey(firstHalfOfString);
+        int secondDKey = getKey(secondHalfOfString);
+        // print the two keys found.
+        System.out.println("Found keys are " + firstDKey + ", " + secondDKey);
+        // Calculate and return the decrypted String using the encryptTwoKeys
+        // method from CaesarCipher class
+        String decrypted = cc.encryptTwoKeys(encrypted, 26 - firstDKey
+                                            , 26 - secondDKey);
+        return decrypted;
+    }
+    
     // Write a testDecrypt method in the CaesarBreaker class that prints
     // the decrypted message, and make sure it works for encrypted messages
     // that were encrypted with one key.
@@ -58,5 +120,26 @@ public class CaesarBreaker {
         System.out.println("decryption is " + decryption);
         // e is 2 times
         System.out.println("answer is     " + answer);
+    }
+    
+    // test halfOfString method
+    public void testHalfOfString () {
+        String message = "Qbkm Zgis";
+        int start = 0;
+        String halfOfString = halfOfString(message, start);
+        System.out.println("String is " + message);
+        System.out.println("half of String is " + halfOfString);
+        
+        start = 1;
+        halfOfString = halfOfString(message, start);
+        System.out.println("String is " + message);
+        System.out.println("half of String is " + halfOfString);
+    }
+    
+    // test decryptTwoKeys
+    public void testDecryptTwoKeys () {
+        FileResource fr = new FileResource();
+        String s = fr.asString();
+        System.out.println("decryption is " + decryptTwoKeys(s));
     }
 }
