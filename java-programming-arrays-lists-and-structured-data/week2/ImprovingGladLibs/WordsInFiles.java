229c81d4d68989eb63f230ef4485c776ba05da21
Yamato Kataoka <yk_time@icloud.com>
2019-10-22 13:34:55 +0900

    added addWordsFromFile method add all the words from f into the map

--- java-programming-arrays-lists-and-structured-data/week2/ImprovingGladLibs/WordsInFiles.java
+++ java-programming-arrays-lists-and-structured-data/week2/ImprovingGladLibs/WordsInFiles.java
@@ -7,6 +7,8 @@
  */
 
 import java.util.*;
+import java.io.File;
+import edu.duke.*;
 
 // Create a new class called WordsInFiles.
 public class WordsInFiles {
@@ -17,5 +19,30 @@ public class WordsInFiles {
     // Write a constructor to initialize the HashMap variable.
     public WordsInFiles () {
         wordsInFilesMap = new HashMap<String, ArrayList<String>>();
+    }
+    
+    // Write a private void method named addWordsFromFile that has
+    // one parameter f of type File.
+    // This method should add all the words from f into the map.
+    private void addWordsFromFile (File f) {
+        FileResource fr = new FileResource(f);
+        for (String s : fr.words()) {
+            // If a word is already in the map,
+            if (wordsInFilesMap.containsKey(s)) {
+                // then add the current filename to its ArrayList
+                int index = wordsInFilesMap.get(s).indexOf(f.getName());
+                // unless the filename is already in the ArrayList.
+                if (index != -1) {
+                    wordsInFilesMap.get(s).add(f.getName());
+                }
+            }
+            // If a word is not in the map, then you must create a
+            // new ArrayList of type String with this word
+            // and have the word map to this ArrayList.
+            else {
+                wordsInFilesMap.put(s, new ArrayList<String>());
+                wordsInFilesMap.get(s).add(f.getName());
+            }
+        }
     }
 }
