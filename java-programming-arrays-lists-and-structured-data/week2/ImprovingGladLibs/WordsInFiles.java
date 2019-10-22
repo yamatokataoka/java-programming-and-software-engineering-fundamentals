848b908931693a425af0fbba3ae2cbe35b521ba9
Yamato Kataoka <yk_time@icloud.com>
2019-10-22 13:14:20 +0900

    added constructor with private Hashmap variable

--- java-programming-arrays-lists-and-structured-data/week2/ImprovingGladLibs/WordsInFiles.java
+++ java-programming-arrays-lists-and-structured-data/week2/ImprovingGladLibs/WordsInFiles.java
@@ -5,6 +5,17 @@
  * @author yamatokataoka
  * @version October 22nd, 2019
  */
-public class WordsInFiles {
 
+import java.util.*;
+
+// Create a new class called WordsInFiles.
+public class WordsInFiles {
+    // Create a private variable to store a HashMap that maps a word to
+    // an ArrayList of filenames.
+    private HashMap<String, ArrayList<String>> wordsInFilesMap;
+    
+    // Write a constructor to initialize the HashMap variable.
+    public WordsInFiles () {
+        wordsInFilesMap = new HashMap<String, ArrayList<String>>();
+    }
 }
