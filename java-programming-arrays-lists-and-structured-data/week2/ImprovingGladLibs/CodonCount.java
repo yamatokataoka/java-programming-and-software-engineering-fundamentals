9093974a7ef3be0c80fe2ae1bf61197d11e79a91
Yamato Kataoka <yk_time@icloud.com>
2019-10-22 12:33:43 +0900

    added tester method on CodonCount.java

--- java-programming-arrays-lists-and-structured-data/week2/ImprovingGladLibs/CodonCount.java
+++ java-programming-arrays-lists-and-structured-data/week2/ImprovingGladLibs/CodonCount.java
@@ -7,6 +7,7 @@
  */
 
 import java.util.*;
+import edu.duke.*;
 
 public class CodonCount {
     private HashMap<String, Integer> dnaCodonCount;
@@ -57,6 +58,44 @@ public class CodonCount {
         for (String s : dnaCodonCount.keySet()) {
             if (start < dnaCodonCount.get(s) && dnaCodonCount.get(s) <= end) {
                 System.out.println(s + "\t" + dnaCodonCount.get(s));
+            }
+        }
+    }
+    
+    // Write a tester method that 
+    public void tester () {
+        // prompts the user for a file that contains a DNA strand.
+        FileResource fr = new FileResource();
+        
+        int start = 1;
+        int end = 5;
+        
+        for (String s : fr.lines()) {
+            String dna = s.toUpperCase().trim();
+            // for each of the three possible reading frames, 
+            for (int k=0; k>3; k++) {
+                // builds a HashMap of codons to their number of occurrences
+                // in the DNA strand
+                buildCodonMap(k, dna);
+                // prints the total number of unique codons in the reading
+                // frame
+                System.out.println("Reading frame starting with "
+                                    + k
+                                    + " results in "
+                                    + " " + dnaCodonCount.size()
+                                    + " unique codons");
+                // prints the most common codon and its count
+                String largestKey = getMostCommonCodon();
+                System.out.println("and most common codon is "
+                                    + largestKey +" with count "
+                                    + dnaCodonCount.get(largestKey));
+                // prints the codons and their number of occurrences for
+                // those codons whose number of occurrences in this reading
+                // frame are between two numbers inclusive.
+                System.out.println("Counts of codons between "
+                                    + start + " and "
+                                    + end + " inclusive are:");
+                printCodonCounts(start, end);
             }
         }
     }
