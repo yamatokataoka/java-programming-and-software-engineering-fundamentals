627f5b2369d56b4fc12850c8f4e149ac0bd79e33
Yamato Kataoka <yk_time@icloud.com>
2019-10-26 22:53:34 +0900

    refactored to have two canvas and global variables

--- programming-foundations-with-javascript-html-and-css/week3/convertanimagetograyscale/dist/script.js
+++ programming-foundations-with-javascript-html-and-css/week3/convertanimagetograyscale/dist/script.js
@@ -1,19 +1,21 @@
 var fileinput = document.getElementById("fileinput");
 var canvas = document.getElementById("canvas");
+var canvasGray = document.getElementById("canvasGray");
 var image;
+var grayimage;
 
 function upload () {
   image = new SimpleImage(fileinput);
+  grayimage = new SimpleImage(fileinput);
   image.drawTo(canvas);
 }
 
 function makeGray () {
-  for (var pixel of image.values()) {
+  for (var pixel of grayimage.values()) {
     var average = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
-    console.log(average);
     pixel.setRed(average);
     pixel.setGreen(average);
     pixel.setBlue(average);
   }
-  image.drawTo(canvas);
+  grayimage.drawTo(canvasGray);
 }
