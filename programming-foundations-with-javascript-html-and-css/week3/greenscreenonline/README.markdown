6754c4a1b68f9144612a066095e904abc9c06ee8
Yamato Kataoka <yk_time@icloud.com>
2019-10-27 18:24:44 +0900

    added greenscreenonline prototype

---
+++ programming-foundations-with-javascript-html-and-css/week3/greenscreenonline/README.markdown
@@ -0,0 +1,4 @@
+# GreenScreenOnline
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/yamatokataoka/pen/WNNOKoR](https://codepen.io/yamatokataoka/pen/WNNOKoR).
+
+ 
---
+++ programming-foundations-with-javascript-html-and-css/week3/greenscreenonline/src/script.js
@@ -0,0 +1,15 @@
+function loadForegroundImage() {
+  alert("loadForegroundImage"); 
+}
+
+function loadBackgroundImage() {
+  alert("loadBackgroundImage"); 
+}
+
+function doGreenScreen() {
+  alert("doGreenScreen"); 
+}
+
+function clearCanvas() {
+  alert("clearCanvas"); 
+}
