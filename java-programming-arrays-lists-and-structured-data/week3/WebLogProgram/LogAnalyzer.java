2f2e1840de0e5db635c371a2b48a347a8b3c03a1
Yamato Kataoka <yk_time@icloud.com>
2019-11-02 13:05:35 +0900

    complete LogAnalyzer class

--- java-programming-arrays-lists-and-structured-data/week3/WebLogProgram/LogAnalyzer.java
+++ java-programming-arrays-lists-and-structured-data/week3/WebLogProgram/LogAnalyzer.java
@@ -14,9 +14,20 @@ public class LogAnalyzer
      private ArrayList<LogEntry> records;
      
      public LogAnalyzer() {
+         // complete the constructor to initialize records to an empty ArrayList
+         records = new ArrayList<LogEntry>();
      }
         
      public void readFile(String filename) {
+         // complete the readFile method to create a FileResource
+         FileResource fr = new FileResource(filename);
+         // iterate over all the lines in the file.
+         for (String s : fr.lines()) {
+             // For each line, create a LogEntry and store it
+             // in the records ArrayList.
+             LogEntry le = WebLogParser.parseEntry(s);
+             records.add(le);
+         }
      }
         
      public void printAll() {
@@ -24,6 +35,4 @@ public class LogAnalyzer
              System.out.println(le);
          }
      }
-     
-     
 }
