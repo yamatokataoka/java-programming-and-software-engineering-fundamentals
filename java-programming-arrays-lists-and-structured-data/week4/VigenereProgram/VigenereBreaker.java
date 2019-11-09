29cb479314ff90b293bd94db07d4dd2e29908e88
Yamato Kataoka <yk_time@icloud.com>
2019-11-09 13:04:27 +0900

    initial commit for VigenereProgram

---
+++ java-programming-arrays-lists-and-structured-data/week4/VigenereProgram/VigenereBreaker.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import edu.duke.*;
+
+public class VigenereBreaker {
+    public String sliceString(String message, int whichSlice, int totalSlices) {
+        //REPLACE WITH YOUR CODE
+        return "WRITE ME!";
+    }
+
+    public int[] tryKeyLength(String encrypted, int klength, char mostCommon) {
+        int[] key = new int[klength];
+        //WRITE YOUR CODE HERE
+        return key;
+    }
+
+    public void breakVigenere () {
+        //WRITE YOUR CODE HERE
+    }
+    
+}
