6f0628c97e855bee75c37777210135456ecdd9a0
Yamato Kataoka <yk_time@icloud.com>
2019-11-09 13:31:29 +0900

    test CaesarCipher class work

--- java-programming-arrays-lists-and-structured-data/week4/VigenereProgram/Tester.java
+++ java-programming-arrays-lists-and-structured-data/week4/VigenereProgram/Tester.java
@@ -5,5 +5,20 @@
  * @author yamatokataoka
  * @version November 9th, 2019
  */
+
+import edu.duke.*;
+
 public class Tester {
+    public void testCaesarCipher () {
+        FileResource fr = new FileResource("VigenereTestData/titus-small.txt");
+        String input = fr.asString();
+        // creates a CaesarCipher object
+        CaesarCipher cc = new CaesarCipher(5);
+        // encrypt
+        String encryption = cc.encrypt(input);
+        System.out.println(encryption);
+        // decrypt
+        String decryption = cc.decrypt(encryption);
+        System.out.println(decryption);
+    }
 }
