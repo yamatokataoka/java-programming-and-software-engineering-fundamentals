19c166cfe999a68cf4c947d97fe1fbea5f86884c
Yamato Kataoka <yk_time@icloud.com>
2019-11-09 13:43:12 +0900

    tested CaesarCracker class

--- java-programming-arrays-lists-and-structured-data/week4/VigenereProgram/Tester.java
+++ java-programming-arrays-lists-and-structured-data/week4/VigenereProgram/Tester.java
@@ -21,4 +21,21 @@ public class Tester {
         String decryption = cc.decrypt(encryption);
         System.out.println(decryption);
     }
+    
+    public void testCaesarCracker () {
+        FileResource fr = new FileResource("VigenereTestData/titus-small_key5.txt");
+        String input = fr.asString();
+        // making a new CaesarCracker object
+        CaesarCracker cc = new CaesarCracker();
+        // decrypting the file titus-small_key5.txt
+        String decryption = cc.decrypt(input);
+        System.out.println(decryption);
+        // and the file oslusiadas_key17.txt, noting that the most common
+        // character in Portuguese is ‘a’.
+        fr = new FileResource("VigenereTestData/oslusiadas_key17.txt");
+        input = fr.asString();
+        cc = new CaesarCracker('a');
+        decryption = cc.decrypt(input);
+        System.out.println(decryption);
+    }
 }
