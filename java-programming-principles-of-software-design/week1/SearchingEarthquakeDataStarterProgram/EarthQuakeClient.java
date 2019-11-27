2bc3f640c7f0549ca6be9d9c3276d9264c6ba5d9
Yamato Kataoka <yk_time@icloud.com>
2019-11-27 09:32:11 +0900

    implemented filterByDistanceFrom method

--- java-programming-principles-of-software-design/week1/SearchingEarthquakeDataStarterProgram/EarthQuakeClient.java
+++ java-programming-principles-of-software-design/week1/SearchingEarthquakeDataStarterProgram/EarthQuakeClient.java
@@ -21,8 +21,11 @@ public class EarthQuakeClient {
     double distMax,
     Location from) {
         ArrayList<QuakeEntry> answer = new ArrayList<QuakeEntry>();
-        // TODO
-
+        for (QuakeEntry qe : quakeData) {
+            if (qe.getLocation().distanceTo(from) < distMax) {
+                answer.add(qe);
+            }
+        }
         return answer;
     }
 
