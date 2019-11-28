ccb28c2a4d2d9180afc82301f494605d952a0c22
Yamato Kataoka <yk_time@icloud.com>
2019-11-28 20:28:24 +0900

    updated findClosestQuakes method

--- java-programming-principles-of-software-design/week1/SearchingEarthquakeDataStarterProgram/ClosestQuakes.java
+++ java-programming-principles-of-software-design/week1/SearchingEarthquakeDataStarterProgram/ClosestQuakes.java
@@ -32,13 +32,14 @@ public class ClosestQuakes {
     public void findClosestQuakes() {
         EarthQuakeParser parser = new EarthQuakeParser();
         //String source = "data/nov20quakedata.atom";
-        String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        String source = "data/nov20quakedatasmall.atom";
         ArrayList<QuakeEntry> list  = parser.read(source);
         System.out.println("read data for "+list.size());
 
         Location jakarta  = new Location(-6.211,106.845);
 
-        ArrayList<QuakeEntry> close = getClosest(list,jakarta,10);
+        ArrayList<QuakeEntry> close = getClosest(list,jakarta,3);
         for(int k=0; k < close.size(); k++){
             QuakeEntry entry = close.get(k);
             double distanceInMeters = jakarta.distanceTo(entry.getLocation());
