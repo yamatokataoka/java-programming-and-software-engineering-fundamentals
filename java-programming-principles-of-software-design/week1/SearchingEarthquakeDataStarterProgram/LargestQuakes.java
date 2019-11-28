5a403ff8b9b59738dc97affb5bb74140ae5c1bf0
Yamato Kataoka <yk_time@icloud.com>
2019-11-29 08:16:38 +0900

    added findLargestQuakes method

--- java-programming-principles-of-software-design/week1/SearchingEarthquakeDataStarterProgram/LargestQuakes.java
+++ java-programming-principles-of-software-design/week1/SearchingEarthquakeDataStarterProgram/LargestQuakes.java
@@ -9,5 +9,16 @@
 import java.util.*;
 
 public class LargestQuakes {
-
+    public void findLargestQuakes() {
+        EarthQuakeParser parser = new EarthQuakeParser();
+        //String source = "data/nov20quakedata.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        String source = "data/nov20quakedatasmall.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        System.out.println("read data for "+list.size());
+        for(int k=0; k < list.size(); k++){
+            System.out.println(list.get(k));
+        }
+        System.out.println("number found: "+list.size());
+    }
 }
