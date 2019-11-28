0043467f8b67ff5ddcb2e5d8a93df569233c5e9b
Yamato Kataoka <yk_time@icloud.com>
2019-11-29 08:31:58 +0900

    implemented indexOfLargest method

--- java-programming-principles-of-software-design/week1/SearchingEarthquakeDataStarterProgram/LargestQuakes.java
+++ java-programming-principles-of-software-design/week1/SearchingEarthquakeDataStarterProgram/LargestQuakes.java
@@ -9,6 +9,17 @@
 import java.util.*;
 
 public class LargestQuakes {
+    public int indexOfLargest(ArrayList<QuakeEntry> data) {
+        int maxIndex = 0;
+        for (int k=0; k<data.size(); k++) {
+            QuakeEntry qe = data.get(k);
+            if (qe.getMagnitude() < data.get(maxIndex).getMagnitude()) {
+                maxIndex = k;
+            }
+        }
+        return maxIndex;
+    }
+
     public void findLargestQuakes() {
         EarthQuakeParser parser = new EarthQuakeParser();
         //String source = "data/nov20quakedata.atom";
@@ -16,9 +27,9 @@ public class LargestQuakes {
         String source = "data/nov20quakedatasmall.atom";
         ArrayList<QuakeEntry> list  = parser.read(source);
         System.out.println("read data for "+list.size());
-        for(int k=0; k < list.size(); k++){
-            System.out.println(list.get(k));
-        }
+        // for(int k=0; k < list.size(); k++){
+        //     System.out.println(list.get(k));
+        // }
         System.out.println("number found: "+list.size());
     }
 }
