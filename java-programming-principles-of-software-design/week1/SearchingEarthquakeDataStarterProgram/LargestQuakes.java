cb52337a61665868c4737da24ac4e8074726ee5d
Yamato Kataoka <yk_time@icloud.com>
2019-11-29 08:11:57 +0900

    added LargestQuakes.java

---
+++ java-programming-principles-of-software-design/week1/SearchingEarthquakeDataStarterProgram/LargestQuakes.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of LargestQuakes here.
+ * 
+ * @author yamatokataoka
+ * @version November 29th, 2019
+ */
+
+import java.util.*;
+
+public class LargestQuakes {
+
+}
