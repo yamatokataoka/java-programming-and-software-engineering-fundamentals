95d1af68e79e810ecdff5cc970ccc093d09aedda
Yamato Kataoka <yk_time@icloud.com>
2019-11-30 09:20:13 +0900

    added starter code

---
+++ java-programming-principles-of-software-design/week1/EarthquakeFilterStarterProgram/EarthQuakeClient2.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import edu.duke.*;
+
+public class EarthQuakeClient2 {
+    public EarthQuakeClient2() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public ArrayList<QuakeEntry> filter(ArrayList<QuakeEntry> quakeData, Filter f) { 
+        ArrayList<QuakeEntry> answer = new ArrayList<QuakeEntry>();
+        for(QuakeEntry qe : quakeData) { 
+            if (f.satisfies(qe)) { 
+                answer.add(qe); 
+            } 
+        } 
+        
+        return answer;
+    } 
+
+    public void quakesWithFilter() { 
+        EarthQuakeParser parser = new EarthQuakeParser(); 
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        String source = "data/nov20quakedatasmall.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);         
+        System.out.println("read data for "+list.size()+" quakes");
+
+        Filter f = new MinMagFilter(4.0); 
+        ArrayList<QuakeEntry> m7  = filter(list, f); 
+        for (QuakeEntry qe: m7) { 
+            System.out.println(qe);
+        } 
+    }
+
+    public void createCSV() {
+        EarthQuakeParser parser = new EarthQuakeParser();
+        //String source = "../data/nov20quakedata.atom";
+        String source = "data/nov20quakedatasmall.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        dumpCSV(list);
+        System.out.println("# quakes read: "+list.size());
+    }
+
+    public void dumpCSV(ArrayList<QuakeEntry> list) {
+        System.out.println("Latitude,Longitude,Magnitude,Info");
+        for(QuakeEntry qe : list){
+            System.out.printf("%4.2f,%4.2f,%4.2f,%s\n",
+                qe.getLocation().getLatitude(),
+                qe.getLocation().getLongitude(),
+                qe.getMagnitude(),
+                qe.getInfo());
+        }
+    }
+
+}
---
+++ java-programming-principles-of-software-design/week1/EarthquakeFilterStarterProgram/MinMagFilter.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class MinMaxFilter here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MinMagFilter implements Filter
+{
+    private double magMin; 
+    
+    public MinMagFilter(double min) { 
+        magMin = min;
+    } 
+
+    public boolean satisfies(QuakeEntry qe) { 
+        return qe.getMagnitude() >= magMin; 
+    } 
+
+}
