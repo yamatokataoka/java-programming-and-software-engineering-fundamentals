43220c7c4f45331578a09f86cd09a0f661edb657
Yamato Kataoka <yk_time@icloud.com>
2019-12-01 10:49:23 +0900

    finished practice quiz

--- java-programming-principles-of-software-design/week2/EarthquakeSortStarterProgram/QuakeSortInPlace.java
+++ java-programming-principles-of-software-design/week2/EarthquakeSortStarterProgram/QuakeSortInPlace.java
@@ -66,6 +66,10 @@ public class QuakeSortInPlace {
             QuakeEntry qmax = in.get(maxIdx);
             in.set(i,qmax);
             in.set(maxIdx,qi);
+            // practice quiz: 1
+            if (i == 50) {
+                return;
+            }
         }
     }
     
@@ -118,16 +122,17 @@ public class QuakeSortInPlace {
         //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
         //String source = "data/nov20quakedatasmall.atom";
         //String source = "data/nov20quakedata.atom";
-        String source = "data/earthquakeDataSampleSix2.atom";
+        //String source = "data/earthquakeDataSampleSix2.atom";
         //String source = "data/earthquakeDataSampleSix1.atom";
+        String source = "data/earthQuakeDataDec6sample1.atom"; // practice quiz: 1, 2, 3
         ArrayList<QuakeEntry> list  = parser.read(source);  
        
         System.out.println("read data for "+list.size()+" quakes");    
         //sortByMagnitude(list);
         //sortByLargestDepth(list);
         //sortByMagnitudeWithBubbleSort(list);
-        //sortByMagnitudeWithBubbleSortWithCheck(list);
-        sortByMagnitudeWithCheck(list);
+        sortByMagnitudeWithBubbleSortWithCheck(list);
+        //sortByMagnitudeWithCheck(list);
         for (QuakeEntry qe: list) { 
             System.out.println(qe);
         } 
