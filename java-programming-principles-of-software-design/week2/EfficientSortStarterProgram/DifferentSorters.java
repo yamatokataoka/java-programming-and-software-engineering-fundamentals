7af31cb5efd9a2832a24902fbd5b58060ef94c9e
Yamato Kataoka <yk_time@icloud.com>
2019-12-01 12:26:04 +0900

    initial commit for EfficientSortStarterProgram

---
+++ java-programming-principles-of-software-design/week2/EfficientSortStarterProgram/DifferentSorters.java
@@ -0,0 +1,49 @@
+
+/**
+ * Write a description of class DifferentSorters here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+
+public class DifferentSorters {
+    public void sortWithCompareTo() {
+        EarthQuakeParser parser = new EarthQuakeParser();
+        String source = "data/nov20quakedata.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        Collections.sort(list);
+        for(QuakeEntry qe: list) {
+            System.out.println(qe);
+        }
+
+    }    
+
+    public void sortByMagnitude() {
+        EarthQuakeParser parser = new EarthQuakeParser();
+        String source = "data/nov20quakedata.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        Collections.sort(list, new MagnitudeComparator());
+        for(QuakeEntry qe: list) {
+            System.out.println(qe);
+        }
+
+    }
+
+    public void sortByDistance() {
+        EarthQuakeParser parser = new EarthQuakeParser();
+        String source = "data/nov20quakedata.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        // Location is Durham, NC
+        Location where = new Location(35.9886, -78.9072);
+        Collections.sort(list, new DistanceComparator(where));
+        for(QuakeEntry qe: list) {
+            System.out.println(qe);
+        }
+
+    }
+}
