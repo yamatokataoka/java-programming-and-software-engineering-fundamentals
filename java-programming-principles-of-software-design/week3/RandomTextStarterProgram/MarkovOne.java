3a7968d3481e41f6fc33821e34cffd963bcd07da
Yamato Kataoka <yk_time@icloud.com>
2019-12-04 08:38:56 +0900

    added MarkovOne class

---
+++ java-programming-principles-of-software-design/week3/RandomTextStarterProgram/MarkovOne.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of MarkovOne here.
+ * 
+ * @author yamatokataoka
+ * @version December 4th, 2019
+ */
+
+import java.util.Random;
+
+public class MarkovOne {
+    private String myText;
+    private Random myRandom;
+
+    public MarkovOne() {
+        myRandom = new Random();
+    }
+
+    public void setRandom(int seed){
+        myRandom = new Random(seed);
+    }
+
+    public void setTraining(String s){
+        myText = s.trim();
+    }
+
+    public String getRandomText(int numChars){
+        if (myText == null){
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int k=0; k < numChars; k++){
+            int index = myRandom.nextInt(myText.length());
+            sb.append(myText.charAt(index));
+        }
+
+        return sb.toString();
+    }
+}
