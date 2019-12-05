dbed09559dce4bd7e7f4e8eab2ce76a7fb61648d
Yamato Kataoka <yk_time@icloud.com>
2019-12-05 19:44:14 +0900

    refactored

--- java-programming-principles-of-software-design/week3/RandomTextStarterProgram/MarkovOne.java
+++ java-programming-principles-of-software-design/week3/RandomTextStarterProgram/MarkovOne.java
@@ -36,18 +36,19 @@ public class MarkovOne {
 
         return sb.toString();
     }
-    
+
     public ArrayList<String> getFollows(String key) {
-        ArrayList<String> list = new ArrayList<String>();
-        int i = 0;
-        while (i<myText.length()-1) {
-            int index = myText.indexOf(key, i);
-            if (index != -1) {
-                String follow = myText.substring(index+1, index+2);
-                list.add(follow);
-                i = index+1;
+        ArrayList<String> follows = new ArrayList<String>();
+        int pos = 0;
+        while (pos < myText.length()-1) {
+            int index = myText.indexOf(key, pos);
+            if (index == -1 || index+key.length() >= myText.length()) {
+                break;
             }
+            String follow = myText.substring(index+key.length(), index+key.length()+1);
+            follows.add(follow);
+            pos = index+key.length();
         }
-        return list;
+        return follows;
     }
 }
