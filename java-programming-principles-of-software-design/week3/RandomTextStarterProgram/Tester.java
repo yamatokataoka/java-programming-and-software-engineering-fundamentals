968b3273a608e60faa7de0499f03ece047d95460
Yamato Kataoka <yk_time@icloud.com>
2019-12-05 19:41:45 +0900

    added test more for getFollows method

--- java-programming-principles-of-software-design/week3/RandomTextStarterProgram/Tester.java
+++ java-programming-principles-of-software-design/week3/RandomTextStarterProgram/Tester.java
@@ -13,8 +13,26 @@ public class Tester {
         MarkovOne markov = new MarkovOne();
         String trainingText = "this is a test yes this is a test.";
         markov.setTraining(trainingText);
+        System.out.println(trainingText.length());
+        // test1
         String key = "t";
         ArrayList<String> follows = markov.getFollows(key);
+        System.out.println(follows.size() + " " + follows);
+        // test2
+        key = "e";
+        follows = markov.getFollows(key);
+        System.out.println(follows.size() + " " + follows);
+        // test3
+        key = "es";
+        follows = markov.getFollows(key);
+        System.out.println(follows.size() + " " + follows);
+        // test4
+        key = ".";
+        follows = markov.getFollows(key);
+        System.out.println(follows.size() + " " + follows);
+        // test5
+        key = "t.";
+        follows = markov.getFollows(key);
         System.out.println(follows.size() + " " + follows);
     }
 }
