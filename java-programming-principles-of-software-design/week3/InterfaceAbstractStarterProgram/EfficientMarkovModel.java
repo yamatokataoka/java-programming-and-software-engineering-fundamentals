b4fec2a3691cfec08736355a3d3ae23ec3e5080a
Yamato Kataoka <yk_time@icloud.com>
2019-12-07 14:16:38 +0900

    implemented buildMap method

--- java-programming-principles-of-software-design/week3/InterfaceAbstractStarterProgram/EfficientMarkovModel.java
+++ java-programming-principles-of-software-design/week3/InterfaceAbstractStarterProgram/EfficientMarkovModel.java
@@ -10,10 +10,12 @@ import java.util.*;
 
 public class EfficientMarkovModel extends AbstractMarkovModel {
     private int n;
+    HashMap<String, ArrayList<String>> followHash;
 
     public EfficientMarkovModel(int num) {
         myRandom = new Random();
         n = num;
+        followHash = new HashMap<String, ArrayList<String>>();
     }
 
     public void setRandom(int seed){
@@ -34,6 +36,7 @@ public class EfficientMarkovModel extends AbstractMarkovModel {
         sb.append(key);
 
         for(int k=0; k < numChars-key.length(); k++){
+            buildMap(key);
             ArrayList<String> follows = getFollows(key);
             if (follows.size() == 0) {
                 break;
@@ -49,5 +52,25 @@ public class EfficientMarkovModel extends AbstractMarkovModel {
 
     public String toString() {
         return "MarkovModel of order " + n;
+    }
+
+    public void buildMap(String key) {
+        if (followHash.containsKey(key)) {
+            return;
+        }
+
+        ArrayList<String> follows = new ArrayList<String>();
+        int pos = 0;
+        while (pos < myText.length()-1) {
+            int index = myText.indexOf(key, pos);
+            if (index == -1 || index+key.length() >= myText.length()) {
+                break;
+            }
+            String follow = myText.substring(index+key.length(), index+key.length()+1);
+            follows.add(follow);
+            pos = index+key.length();
+        }
+
+        followHash.put(key, follows);
     }
 }
