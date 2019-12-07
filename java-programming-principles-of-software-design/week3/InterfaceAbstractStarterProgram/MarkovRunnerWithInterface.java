5845287f76109cd4f806ba5e55e99dc05b6769ea
Yamato Kataoka <yk_time@icloud.com>
2019-12-07 14:45:21 +0900

    formatted

--- java-programming-principles-of-software-design/week3/InterfaceAbstractStarterProgram/MarkovRunnerWithInterface.java
+++ java-programming-principles-of-software-design/week3/InterfaceAbstractStarterProgram/MarkovRunnerWithInterface.java
@@ -14,18 +14,18 @@ public class MarkovRunnerWithInterface {
         markov.setRandom(seed);
         System.out.println("running with " + markov);
         for(int k=0; k < 3; k++){
-			String st= markov.getRandomText(size);
-			printOut(st);
-		}
+            String st= markov.getRandomText(size);
+            printOut(st);
+        }
     }
-    
+
     public void runMarkov() {
         FileResource fr = new FileResource();
         String st = fr.asString();
         st = st.replace('\n', ' ');
         int size = 200;
         int seed = 10;
-		
+        
         MarkovZero mz = new MarkovZero();
         runModel(mz, st, size, seed);
     
@@ -40,19 +40,18 @@ public class MarkovRunnerWithInterface {
 
     }
 
-	private void printOut(String s){
-		String[] words = s.split("\\s+");
-		int psize = 0;
-		System.out.println("----------------------------------");
-		for(int k=0; k < words.length; k++){
-			System.out.print(words[k]+ " ");
-			psize += words[k].length() + 1;
-			if (psize > 60) {
-				System.out.println();
-				psize = 0;
-			}
-		}
-		System.out.println("\n----------------------------------");
-	}
-	
+    private void printOut(String s){
+        String[] words = s.split("\\s+");
+        int psize = 0;
+        System.out.println("----------------------------------");
+        for(int k=0; k < words.length; k++){
+            System.out.print(words[k]+ " ");
+            psize += words[k].length() + 1;
+            if (psize > 60) {
+                System.out.println();
+                psize = 0;
+            }
+        }
+        System.out.println("\n----------------------------------");
+    }
 }
