c9e33452710c474ff51d4261c9c56ea04bfadbb8
Yamato Kataoka <yk_time@icloud.com>
2019-12-07 22:48:32 +0900

    added sinature and formatted

--- java-programming-principles-of-software-design/week3/WordNGramStarterProgram/MarkovWordOne.java
+++ java-programming-principles-of-software-design/week3/WordNGramStarterProgram/MarkovWordOne.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class MarkovWordOne here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author yamato
+ * @version December 7th, 2019
  */
 
 import java.util.*;
@@ -11,43 +11,43 @@ import java.util.*;
 public class MarkovWordOne implements IMarkovModel {
     private String[] myText;
     private Random myRandom;
-    
+
     public MarkovWordOne() {
         myRandom = new Random();
     }
-    
+
     public void setRandom(int seed) {
         myRandom = new Random(seed);
     }
-    
+
     public void setTraining(String text){
-		myText = text.split("\\s+");
-	}
-	
-	public String getRandomText(int numWords){
-		StringBuilder sb = new StringBuilder();
-		int index = myRandom.nextInt(myText.length-1);  // random word to start with
-		String key = myText[index];
-		sb.append(key);
-		sb.append(" ");
-		for(int k=0; k < numWords-1; k++){
-		    ArrayList<String> follows = getFollows(key);
-		    if (follows.size() == 0) {
-		        break;
-		    }
-			index = myRandom.nextInt(follows.size());
-			String next = follows.get(index);
-			sb.append(next);
-			sb.append(" ");
-			key = next;
-		}
-		
-		return sb.toString().trim();
-	}
-	
-	private ArrayList<String> getFollows(String key) {
-	    ArrayList<String> follows = new ArrayList<String>();
-	    return follows;
+        myText = text.split("\\s+");
+    }
+
+    public String getRandomText(int numWords){
+        StringBuilder sb = new StringBuilder();
+        int index = myRandom.nextInt(myText.length-1);  // random word to start with
+        String key = myText[index];
+        sb.append(key);
+        sb.append(" ");
+        for(int k=0; k < numWords-1; k++){
+            ArrayList<String> follows = getFollows(key);
+            if (follows.size() == 0) {
+                break;
+            }
+            index = myRandom.nextInt(follows.size());
+            String next = follows.get(index);
+            sb.append(next);
+            sb.append(" ");
+            key = next;
+        }
+
+        return sb.toString().trim();
+    }
+
+    private ArrayList<String> getFollows(String key) {
+        ArrayList<String> follows = new ArrayList<String>();
+        return follows;
     }
 
 }
