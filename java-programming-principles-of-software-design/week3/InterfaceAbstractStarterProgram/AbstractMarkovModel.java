e16521db7522a9efc33bf9b31037609d635aeb7d
Yamato Kataoka <yk_time@icloud.com>
2019-12-07 13:11:24 +0900

    added getFollows method

--- java-programming-principles-of-software-design/week3/InterfaceAbstractStarterProgram/AbstractMarkovModel.java
+++ java-programming-principles-of-software-design/week3/InterfaceAbstractStarterProgram/AbstractMarkovModel.java
@@ -22,4 +22,18 @@ public abstract class AbstractMarkovModel implements IMarkovModel {
  
     abstract public String getRandomText(int numChars);
 
+    protected ArrayList<String> getFollows(String key) {
+        ArrayList<String> follows = new ArrayList<String>();
+        int pos = 0;
+        while (pos < myText.length()-1) {
+            int index = myText.indexOf(key, pos);
+            if (index == -1 || index+key.length() >= myText.length()) {
+                break;
+            }
+            String follow = myText.substring(index+key.length(), index+key.length()+1);
+            follows.add(follow);
+            pos = index+key.length();
+        }
+        return follows;
+    }
 }
