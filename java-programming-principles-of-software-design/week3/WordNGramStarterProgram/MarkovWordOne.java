0428dbdf06951f5edb5ae9f84e7221382faca3e2
Yamato Kataoka <yk_time@icloud.com>
2019-12-07 19:29:49 +0900

    initial commit for WordNGramStarterProgram

---
+++ java-programming-principles-of-software-design/week3/WordNGramStarterProgram/MarkovWordOne.java
@@ -0,0 +1,53 @@
+
+/**
+ * Write a description of class MarkovWordOne here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+
+public class MarkovWordOne implements IMarkovModel {
+    private String[] myText;
+    private Random myRandom;
+    
+    public MarkovWordOne() {
+        myRandom = new Random();
+    }
+    
+    public void setRandom(int seed) {
+        myRandom = new Random(seed);
+    }
+    
+    public void setTraining(String text){
+		myText = text.split("\\s+");
+	}
+	
+	public String getRandomText(int numWords){
+		StringBuilder sb = new StringBuilder();
+		int index = myRandom.nextInt(myText.length-1);  // random word to start with
+		String key = myText[index];
+		sb.append(key);
+		sb.append(" ");
+		for(int k=0; k < numWords-1; k++){
+		    ArrayList<String> follows = getFollows(key);
+		    if (follows.size() == 0) {
+		        break;
+		    }
+			index = myRandom.nextInt(follows.size());
+			String next = follows.get(index);
+			sb.append(next);
+			sb.append(" ");
+			key = next;
+		}
+		
+		return sb.toString().trim();
+	}
+	
+	private ArrayList<String> getFollows(String key) {
+	    ArrayList<String> follows = new ArrayList<String>();
+	    return follows;
+    }
+
+}
